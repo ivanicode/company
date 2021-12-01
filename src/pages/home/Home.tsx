5cd5e154d68a79ec41e9534ea06dc1a1124025a8
ivani <katarzyna.stosio@gmail.com>
2021-12-01 18:59:08 +0100

    home content

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,18 +1,24 @@
-
+import './home.css'
 
 function Home() {
     return (
-        <>
-            <div>
-            Home
+        <div className="homePage">
+            <div className="description one">
+                
+                Here is a place for a short description of what the company does. How great employees it has and what wonderful offers or services it offers.
+                
             </div>
-            <div>
-            Home
+            <div className="description two">
+                
+                Here a few words, not necessarily true, about easy communication with the company as well as detailed valuations and precise formalities.
+                
             </div>
-            <div>
-            Home
+            <div className="description three">
+                
+                And here we invite the client to a non-binding conversation with us. And the home page is ready!
+                
             </div>
-        </>
+        </div>
     )
 }
 
